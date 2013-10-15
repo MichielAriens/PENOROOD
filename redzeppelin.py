87503c49d8ac34958bf75f36b709534fd625b3f8
MichielAriens <michiel.ariens@student.kuleuven.be>
2013-10-15 21:44:03 +0200

    Een eerste aanzet met de camera

--- redzeppelin.py
+++ redzeppelin.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 
+#numpy and scipy will need to be installed on the PI for some code to work.
+
 #The top level zeppelin code. 
 
 import distSensor
 
 #testcode to check the distSensor code
 
-continuebool = True
-distSensor = distSensor.FakeDistanceSensor()
-counter = 1
 
-
-while continuebool:
-    height = distSensor.getHeight()
-    print str(counter) + ": " + str(height)
-    counter += 1
-    cont = raw_input("continue? ('n' to stop): ")
-    if(cont == "n"):
-        continuebool = False
+def testDistSens():
+    continuebool = True
+    distSensor = distSensor.FakeDistanceSensor()
+    counter = 1
+    
+    
+    while continuebool:
+        height = distSensor.getHeight()
+        print str(counter) + ": " + str(height)
+        counter += 1
+        cont = raw_input("continue? ('n' to stop): ")
+        if(cont == "n"):
+            continuebool = False
+            
+testDistSens()
     
     
         
