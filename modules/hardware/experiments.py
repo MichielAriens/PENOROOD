774c5a4b86ab7419d2894fc1e4f5101b03013f23
MichielAriens <michiel.ariens@student.kuleuven.be>
2013-10-21 16:01:08 +0200

    experiments code

--- modules/hardware/experiments.py
+++ modules/hardware/experiments.py
@@ -2,3 +2,23 @@ import distSensor
 
 ds = distSensor.DistanceSensor()
 
+file = open("./data/esp.csv","w")
+
+max = 100
+repeats = 100
+
+i = 0
+while i < max:
+    file.write(str(i) + ";")
+    j=0
+    while j < repeats:
+        file.write(str(ds.getHeightRaw(i)) + ";")
+        j += 1
+        
+    file.write("\n")
+    i += 1
+    
+
+    
+    
+        
