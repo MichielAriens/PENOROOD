acfc4d2dd997c9e026a5a3e903ba7bb1ea8ab81e
MichielAriens <michiel.ariens@student.kuleuven.be>
2013-11-05 15:14:32 +0100

    errors more

--- modules/lowLevelController.py
+++ modules/lowLevelController.py
@@ -21,8 +21,7 @@ class LowLevelController:
         self.pid.setPoint(self.dHeight)
         
         if simMode == "RPi":
-            self.altimeter = ds.DistanceSens
-            or()
+            self.altimeter = ds.DistanceSensor()
             self.lift = motor.VectoredMotor(24,4)
             compMotor = motor.CompositeMotor(motor.PulsedMotor(17,23), motor.PulsedMotor(9,7))
             self.thrust = compMotor.thruster
