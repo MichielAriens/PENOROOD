b56214b3e620e45df30ba2bb8dd7c5b2b1617817
MichielAriens <michiel.ariens@hotmail.com>
2013-11-22 13:55:40 +0000

    bugfix

--- preempt.py
+++ preempt.py
@@ -1,14 +1,14 @@
 import time
 from decimal import *
 
-count = 100
+count = 10000
 
 
 prev = Decimal(time.time())
 while(count > 0):
     now = Decimal(time.time())
-    print str(now - prev)
-    b = 0 == (now - prev)
+    #print str(now - prev)
+    b = 0.01 >=  (now - prev)
     if not b:
         print "An interrupt happened"
     prev = now
