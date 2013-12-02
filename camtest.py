a50d6365ebe4f505452aa85154d809b6de72d6aa
MichielAriens <michiel.ariens@student.kuleuven.be>
2013-12-02 17:11:03 +0100

    Working stills

--- camtest.py
+++ camtest.py
@@ -3,5 +3,7 @@ import modules.hardware.camera as cam
 
 mycam = cam.Camera(height = 500, width = 500)
 while(True):
-    x= raw_input("go")
+    x = raw_input("go")
+    mycam.click()
+    
     
--- modules/hardware/camera.py
+++ modules/hardware/camera.py
@@ -9,26 +9,23 @@ import os
 import thread
 import time
 
-class Camera:
-    cam = None
-    
+class Camera:    
     def __init__(self,height = 200, width = 200, output = "still.jpg", root = "data/cam/", readroot = "images/"):
         self.height = width
         self.width = width      
         self.output = output  
         self.root = root
         self.readroot = readroot
-        return
     
     
     #launches repeating 
-    def cick(self):
+    def click(self):
         #command = "raspistill -w " + str(self.width) + " -h " + str(self.height)+ " -q 5 -o " + self.root + self.output + " -t 9999999 -th 0:0:0 -tl 100 -n &"
-        command = "raspistill -w " + str(self.width) + " -h " + str(self.height)+ " -q 5 -o " + self.root + self.output + " -t 0.001 -n &"
+        command = "raspistill -w " + str(self.width) + " -h " + str(self.height)+ " -o " + self.root + self.output + " -t 0.001 -n"
         print "trying: " + command
         os.system(command)
         timestamp = time.time()
-        tsfile = open(self.root + "TIMESTAMP")
+        tsfile = open(self.root + "TIMESTAMP",'w')
         tsfile.write(str(timestamp))
         tsfile.close()
         
