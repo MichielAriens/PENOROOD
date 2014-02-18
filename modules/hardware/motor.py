90e39c8ac617f7f1f70d210f2f09a9dbe4056f12
AlexanderTang <alexandertang357@hotmail.com>
2014-02-18 16:58:16 +0100

    Update motor.py

--- modules/hardware/motor.py
+++ modules/hardware/motor.py
@@ -2,65 +2,31 @@ import time
 import thread
 try:
     import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)                      # choose BCM or BOARD 
 except:
     pass
 
-#A logical motor provides an abstraction layer above the specific layout of the motors. 
-#specifically a logical motor is a rudder or a thruster. It relies on a composite thruster to distribute the thrust among the
-#real engines. This class should not be used by the user: it should be deemed private.
-#The only purpose of this class is actually to handle exceptions (abs(thrust)>100)
-class LogicalMotor:
-    def __init__(self,master):
-        self.master = master
-        self.thrust = 0
-        
-        
-    def setThrust(self,nThrust):
-        if(nThrust > 100):
-            self.thrust = 100.0
-        elif(nThrust < -100):
-            self.thrust = -100.0
-        else:
-            self.thrust = nThrust
-        self.master.setThrust()
-        
-#A class that converts logical motors like the rudder and actuates that into real motors.
-#Defines two logical classes: rudder and thruster which can be used as normal motors.
-#Rudder is clockwise for increasing thrust values
-class CompositeMotor:
-    #Takes two real motor implementations. These should be the two thrusters on the wings
-    def __init__(self,leftMotor,rightMotor):
-        #define the real motors
-        self.leftMotor = leftMotor
-        self.rightMotor = rightMotor
-        #define the logical motors
-        self.rudder = LogicalMotor(self)
-        self.thruster = LogicalMotor(self)
-        #determines strength of the rotation
-        self.rotationFactor = 1
-    
-    def setThrust(self):
-        self.leftMotor.setThrust(self.thruster.thrust + self.rudder.thrust)
-        self.rightMotor.setThrust(self.thruster.thrust - self.rudder.thrust)
         
 #A physical implementation of a motor. Uses PWM (variable output pins) to drive motors
 #Provides best control. Useful for most critical motors like the lifter.
-class VectoredMotor:
+class PWMMotor:
     def __init__(self,positivePin,negativePin):
         #Instantiate pin objects
-        #--Placeholder--
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.OUT)
-        self.thrustPin = GPIO.PWM(18, 50)
+        
+        GPIO.setup(18, GPIO.OUT)                    # set a port/pin (18) as an output 
+        self.thrustPin = GPIO.PWM(18, 50)           # create an object thrustPin for PWM on port 18 at 50 Hertz (maximum 1k Hz I think)
         self.positivePin = positivePin
         self.negativePin = negativePin
         self.thrust = 0
+        
         GPIO.setup(self.positivePin,GPIO.OUT)
         GPIO.setup(self.negativePin,GPIO.OUT)
-        GPIO.output(self.positivePin,True)
-        GPIO.output(self.negativePin,False)
+        GPIO.output(self.positivePin,True)          # set port/pin value to 1/GPIO.HIGH/True  
+        GPIO.output(self.negativePin,False)         # set port/pin value to 0/GPIO.LOW/False  
         
-        self.thrustPin.start(0.0)
+        self.thrustPin.start(0.0)                    # start the PWM on 0 percent duty cycle
+                                                     # explanation duty cycles: http://raspi.tv/2013/rpi-gpio-0-5-2a-now-has-software-pwm-how-to-use-it
+                                                     # may vary from 0 to 100 (obviously)
             
     def setThrust(self,nThrust):
         if(nThrust > 100):
@@ -72,29 +38,33 @@ class VectoredMotor:
         self._actuate()
         
     def _actuate(self):
-        GPIO.setmode(GPIO.BCM)
+        #decide direction
         if(self.thrust < 0):
             GPIO.output(self.positivePin,False)
             GPIO.output(self.negativePin,True)
         else:
             GPIO.output(self.positivePin,True)
             GPIO.output(self.negativePin,False)
+            
+        #decide power of thrust
         self.thrustPin.ChangeDutyCycle(abs(self.thrust))
 
 #A pysical implementation of a motor. Uses fixed pins but still provides a way to pass real numbers to setThrust()
 #Runs a thread in the background and runs the motors on max power for abs(thrust) percent of the time.
+#Cool note: PWM works on the exact same principle!
 class PulsedMotor:
     def __init__(self,postivePin,negativePin):
         #Instantiate pin objects
-        #--Placeholder--
-        GPIO.setmode(GPIO.BCM)
+
         self.positivePin = postivePin
         self.negativePin = negativePin
+        
         GPIO.setup(self.positivePin,GPIO.OUT)
         GPIO.setup(self.negativePin,GPIO.OUT)
         GPIO.output(self.positivePin,False)
         GPIO.output(self.negativePin,False)
         self.thrust = 0
+        
         thread.start_new(self.pulse, (1000,1))
         
     def setThrust(self,nThrust):
@@ -145,8 +115,3 @@ class FakeMotor:
         else:
             self.thrust = thrust
         self.env.verticalForce = self.thrust/100 * self.MAXFORCE
-        
-class EmptyMotor:
-    
-    def setThrust(self,ignore):
-        return        
