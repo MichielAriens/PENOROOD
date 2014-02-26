6b22e614ca6b7572c623af603c15f673c26cafc4
AlexanderTang <alexandertang357@hotmail.com>
2014-02-26 16:44:53 +0100

    Hij kan rechthoeken en cirkels zeer goed herkennen (kleine variaties mogelijk zoals zeer lichte afwijking naar ellips of rechthoek met ronde hoeken)

--- RPI/software/shapeRecognition.py
+++ RPI/software/shapeRecognition.py
@@ -12,15 +12,57 @@ from SimpleCV import Color, Image
 # 2) determine shape with SimpleCV.Features.Blob.Blob isCircle isRectangle isSquare method
 #       (define own class for star and heart shape)
 
-
-image = Image('C:\Users\Babyburger\Desktop\shapes.jpg')
+# Find the blobs in a given picture
+image = Image('C:\Users\Babyburger\Desktop\whiteshapes.jpg')
 binarized = image.binarize(220).invert()
 blobs = binarized.findBlobs()
+
+"""
+# Shows the distinguished blobs, give a random input for each iteration
 for blob in blobs:
     blob.draw()
     print blob
+    if (blob.isCircle(0.2) == True): print "circle"
+    if (blob.isRectangle(0.05) == True): print "rectangle"
     binarized.show()
     raw_input()
+"""
+
+print type(blobs)
+
+# Find circles
+circleTolerance = 0.2  # 0.2 may be a bit too large, but 0.05 is definately too small.  Further testing required.
+circles = [b for b in blobs if b.isCircle(circleTolerance)]  # find all the circles within a certain tolerance
+print type(circles)
+for circle in circles:
+    circle.draw()
+    print circle
+    binarized.show()
+    raw_input()
+
+
+"""
+# Find rectangles
+rectangleTolerance = 0.05  # Keep this number very low or every figure becomes a rectangle...
+rectangles = next(b for b in blobs if b.isRectangle(rectangleTolerance))  # find all the rectangles within a certain tolerance
+for rectangle in rectangles:
+    rectangle.draw()
+    print circle
+    binarized.show()
+    raw_input()
+"""
+
+
+"""
+# General method for drawing found figures.
+def findFigure(self,figures):
+    self.figure = figures
+    for figure in figures:
+        figure.draw()
+        print figure
+        binarized.show()
+        raw_input()
+"""
 
 
 # filtering method: http://stackoverflow.com/questions/14036944/how-do-i-locate-the-rabbit
