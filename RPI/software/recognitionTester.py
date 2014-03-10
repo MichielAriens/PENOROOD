e4a8b836927d9f3e0265ad4582ba2b9e26aff3d4
AlexanderTang <alexandertang357@hotmail.com>
2014-03-11 00:43:34 +0100

    Adjusted filtering

--- RPI/software/recognitionTester.py
+++ RPI/software/recognitionTester.py
@@ -1,3 +1,6 @@
+# Using a method to determine hsv per pixel is getting cpu intensive and complicated fast.
+# Use http://www.rapidtables.com/convert/color/hsv-to-rgb.htm to experiment with the color ranges.
+
 import shapeRecognition as sr
 
 shapes = sr.ShapeFinder()
