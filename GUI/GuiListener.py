44afcf2a04afc79273faeb4ec1f62e1d42682bfa
SimonDierckx <simon_dierckx@hotmail.com>
2014-04-11 13:39:22 +0200

    sddsqfd

--- GUI/GuiListener.py
+++ GUI/GuiListener.py
@@ -9,25 +9,23 @@ class GuiListener:
         self.zepListener = None
         self.gui = None
         self.zepID = None
-        self.socket = None
-        try:
-            HOST, PORT = 'localhost', 21567
-            # SOCK_STREAM == a TCP socket
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #sock.setblocking(0)  # optional non-blocking
-            print(sock)
-            sock.connect((HOST, PORT))
-            self.socket = sock
-        except:
-            print("Could not connect to server.")
+        self.connected = False
+        self.connect()
+        
+        
+    def connect(self):
+        pass
         
     def link(self,zepListener):
         self.zepListener = zepListener
         
     def getPosition(self):
-        if(self.zepListener is not None):
+        if(not self.connected):
             pos = self.zepListener.getPosition()
-            self.getPositionServer()
+            
+            #test
+            testpos = self.getPositionMQ()
+            
             return pos
         else:
             print("Could not get position of zep (ID=" + str(self.zepID) + ").")
@@ -46,18 +44,108 @@ class GuiListener:
     def setZepID(self, ID):
         self.zepID = ID
         
-    def getPositionServer(self):
-        if(self.socket is not None):
+    def getPositionMQ(self):
+        if(self.connected):
             try:
-                #test
-                sendMessage("G=1", self.socket)
-                print("was able to send")
-                reply = sock.recv(16384)  # limit reply to 16K
-                print(reply.decode("utf-8"))
-                print("SERVER REPLY: " + multithread_client.client("G==1"))
+                pass
             except:
                 print("not working")
                 return (-1,-1)
-            
     
+    def communicateWithMQ(self):
+        pass
+    
+    def getLocationMQ(self,x,y,color):
+        return color + ".info.location" + str(x) + "," + str(y)  
+    
+    def getHeightMQ(self,z,color):
+        return color + ".info.height" + str(z)
+    
+    def moveMQ(self,x,y, color):
+        return color + ".hcommand.move" + str(x) + "," + str(y)  
+    
+    def elevateMQ(self,z, color):
+        return color + ".hcommand.elevate" + "<" + str(z)
+    
+    def commandMotor1MQ(self, percentage, color):
+        return color + ".lcommand.motor1" + str(percentage)
+    
+    def commandMotor2MQ(self, percentage, color):
+        return color + ".lcommand.motor1" + str(percentage)
+    
+    def commandMotor3MQ(self, percentage, color):
+        return color + ".lcommand.motor1" + str(percentage)
+    
+    def privateMQ(self, command, color):
+        return color + "." + command
+    
+    def decodeResponse(self, command):
+        split = command.string.rsplit(".");
+        color = split[0]
+        try:
+            if(split[1] == "info"):
+                if(split[2] == "height"):
+                    height = split[3]
+                    print(color + ".info.height."+height)
+                elif(split[2] == "location"):
+                    location = split[3]
+                    print(color + ".info.height."+location)
+                else:
+                    pass
+            elif(split[1] == "hcommand"):
+                if(split[2] == "move"):
+                    move = split[3]
+                    print(color + ".hcommand.move."+ move)
+                elif(split[2] == "elevate"):
+                    elevate = split[3]
+                    print(color + ".hcommand.elevate."+ elevate)
+                else:
+                    pass
+            elif(split[1] == "lcommand"):
+                if(split[2] == "motor1"):
+                    power = split[3]
+                    print(color + ".lcommand.motor1."+ power)
+                elif(split[2] == "motor2"):
+                    power = split[3]
+                    print(color + ".lcommand.motor2."+ power)
+                elif(split[2] == "motor3"):
+                    power = split[3]
+                    print(color + ".lcommand.motor3."+ power)
+            elif(split[1] == "private"):
+                pass
+            else:
+                pass
+            
+        except:
+            print("Something wrong happened at decodeResponse() in GuiListener.")
         
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
