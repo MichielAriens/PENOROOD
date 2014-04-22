4c8b7e14fbb43bb12f06fffb1851160f074fab40
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-04-22 18:24:28 +0200

    xwxwdjsjldjksqd

--- pi/ZepListener.py
+++ pi/ZepListener.py
@@ -3,20 +3,19 @@ import pika
 class zepListener:
 	def __init__(self, zeppelin):
 		self.zeppelin = zeppelin
+		init()
 		
-		
+	def callback(ch, method, properties, body):
+		print(" [x] Received %r" % (body,))
 
+	def init(self):
 		creds = pika.PlainCredentials('rood','rood')
 		connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',credentials=creds ))
 		channel = connection.channel()
 		channel.queue_declare(queue='hellorood')
 		channel.basic_consume(callback,queue='hellorood',no_ack=True)
 		channel.start_consuming()
-		
-	def callback(ch, method, properties, body):
-		print(" [x] Received %r" % (body,))
-		
-				
+			
 	def pushPosition(self,pos):
 		key = "rood.info.location"  
 		myBody = pos[0]*10 + "," + pos[1]*10
