da1ad771648b9c8f8c89f10b23eebfb99ddc0758
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-04-24 16:15:34 +0200

    h

--- pi/distSensor.py
+++ pi/distSensor.py
@@ -414,7 +414,7 @@ class BackgroundDistanceSensor :
         self.points = [0] * size        
         while True:
             height = self.measure()
-            print "Single measure result : " + height
+            print "Single measure result : " + str(height)
             if (height != -1):
                 self.points.pop(0)
                 self.points.append(height)
