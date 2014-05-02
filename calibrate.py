b490342425902bbac2d71128afe3771246b47c95
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-05-02 16:53:30 +0200

    sqdqsd

--- calibrate.py
+++ calibrate.py
@@ -2,7 +2,7 @@ import RPI.software.recognition.main as imp
 import picamera
 
 path = "/home/pi/zep2/output/path.jpg"
-
+res = 250
 
 with picamera.PiCamera() as camera:
         camera.resolution = (res,res)
