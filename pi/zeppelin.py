c57eab93212de184cd9b46f96121d5d17ce56d0f
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-05-05 21:54:34 +0200

    qdsqd

--- pi/zeppelin.py
+++ pi/zeppelin.py
@@ -44,8 +44,8 @@ class Zeppelin:
         #self.xMot = motor.PulsedMotor(17,23)
         #self.yMot = motor.PulsedMotor(9,7)
 
-        self.motorY = motor.PulsedMotor(24,4)
-        self.motorX = motor.PulsedMotor(17,23)
+        self.motorY = motor.simpleMotor(24,4)
+        self.motorX = motor.simpleMotor(17,23)
         self.lift = motor.PWMMotor(9,7)
 
         print "100"
