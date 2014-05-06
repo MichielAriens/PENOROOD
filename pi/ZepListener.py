aff82d4da6fdc721209e0548224efa4c4a59db06
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-05-06 11:42:38 +0200

    dsjflksdjflds

--- pi/ZepListener.py
+++ pi/ZepListener.py
@@ -32,7 +32,7 @@ class zepListener:
             if parts[0] == "rood":
                 #print "got a message"
                 if parts[1] == "private":
-                    if parts[2] == "qr"
+                    if parts[2] == "qr":
                         self.zeppelin.completeQR(int(body))
                     if parts[2] == "override":
                         if str(body) == "true":
